1ea2b8a6a8ce8f19140c6ea7c4fbe9d4c57628a5
Đăng Thức Nguyễn <thuc.nguyendang@hcmut.edu.vn>
2024-04-19 05:42:23 +0700

    update

--- utils/data_preparing.py
+++ utils/data_preparing.py
@@ -1,18 +1,21 @@
 import csv
 from .data_prepropressing import preprocessing
 
-def prepare_data():
+import csv
+from .data_preprocessing import preprocessing
+
+def prepare_data(file_paths):
     print("Preparing data...")
     x_data = []
     y_data = []
-    with open('data/reviews_data.csv', 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        
-        for row in reader:
-            word_list = preprocessing(row[0])
-            label = int(row[1])
-            if len(word_list) > 0:
-                x_data.append(word_list)
-                y_data.append(label)
-    
+    for file_path in file_paths:
+        print(f"Processing data from file: {file_path}")
+        with open(file_path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                word_list = preprocessing(row[0])
+                label = int(row[1])
+                if len(word_list) > 0:
+                    x_data.append(word_list)
+                    y_data.append(label)
     return x_data, y_data
